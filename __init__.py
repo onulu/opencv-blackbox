2a9d2c833bc93b10bd411d1c40c9645e248eac2e
onulu <123046744+onulu@users.noreply.github.com>
2024-09-02 16:53:21 +0900

    init project

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+project_name package.
+
+This package contains modules for [brief description of your project].
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+# from .module1 import SomeClass
+# from .module2 import some_function
+
+# You can define __all__ to control what's imported with "from project_name import *"
+# __all__ = ["SomeClass", "some_function"]
